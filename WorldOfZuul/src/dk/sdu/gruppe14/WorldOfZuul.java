b9e73f2cc6f803012fbc79ae461c0c6f09483f43
Jollybobs <jonathanphj@hotmail.com>
2017-09-24 21:40:52 +0200

    - game.play() now executed in main().

--- WorldOfZuul/src/dk/sdu/gruppe14/WorldOfZuul.java
+++ WorldOfZuul/src/dk/sdu/gruppe14/WorldOfZuul.java
@@ -5,6 +5,8 @@
  */
 package dk.sdu.gruppe14;
 
+import com.framework.WorldOfZuul.Game;
+
 /**
  *
  * @author Jonathan
@@ -16,6 +18,8 @@ public class WorldOfZuul {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Game game = new Game();
+        game.play();
     }
     
 }
