8057b725fb9808b2c6ce2e4f7543b0216e516970
Jollybobs <jonathanphj@hotmail.com>
2017-09-26 07:56:09 +0200

    jUnit test class

--- WorldOfZuul/test/dk/sdu/group14/RoomTest.java
+++ WorldOfZuul/test/dk/sdu/group14/RoomTest.java
@@ -3,11 +3,24 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package test;
+package dk.sdu.group14;
+
+import org.junit.Test;
+
 /**
  *
  * @author jphj
  */
 public class RoomTest {
+    
+    /**
+     * Testing rooms
+     * 
+     * @throws Exception 
+     */
+    @Test
+    public void testRooms() throws Exception {
+        
+    }
     
 }
