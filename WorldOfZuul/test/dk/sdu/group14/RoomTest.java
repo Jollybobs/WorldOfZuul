5ac783d2cb50b01ddce80b592b3775a69f421fb5
Jollybobs <jonathanphj@hotmail.com>
2017-09-26 08:52:38 +0200

    jUnit test class

--- WorldOfZuul/test/dk/sdu/group14/RoomTest.java
+++ WorldOfZuul/test/dk/sdu/group14/RoomTest.java
@@ -1,17 +1,25 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package dk.sdu.group14;
 
+import org.junit.Before;
 import org.junit.Test;
+
+import com.framework.WorldOfZuul.Room;
 
 /**
  *
  * @author jphj
  */
 public class RoomTest {
+    
+    Room aRoom1;
+    Room aRoom2;
+    
+    @Before
+    public void setup() {
+        
+        aRoom1 = new Room("roomStart");
+        aRoom2 = new Room("roomEast");
+    }
     
     /**
      * Testing rooms
@@ -20,7 +28,8 @@ public class RoomTest {
      */
     @Test
     public void testRooms() throws Exception {
-        
+        aRoom1.setExit("east", aRoom1);
+        aRoom1.getExit("east");
     }
     
 }
