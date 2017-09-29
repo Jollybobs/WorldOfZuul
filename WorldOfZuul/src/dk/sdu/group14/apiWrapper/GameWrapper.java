6508b9e1812d5fd49f3f15bc063720c43cf74640
Jollybob <jonathanphj@icloud.com>
2017-09-29 08:56:16 +0200

    API Wrapper

---
+++ WorldOfZuul/src/dk/sdu/group14/apiWrapper/GameWrapper.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dk.sdu.group14.apiWrapper;
+
+import com.framework.WorldOfZuul.Game;
+
+/**
+ *
+ * @author jollybob
+ */
+public class GameWrapper {
+    Game aGame = new Game();
+
+    public void play() 
+    {            
+        aGame.play();
+    }
+}
