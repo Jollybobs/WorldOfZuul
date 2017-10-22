b84ab37c5d14a2793bc587f5330743388f9cefea
103020 <kierkegaardchristian@gmail.com>
2017-10-22 14:19:49 +0200

    Made so the guard can move in his patrol

--- WorldOfZuul/src/com/framework/WorldOfZuul/Guard.java
+++ WorldOfZuul/src/com/framework/WorldOfZuul/Guard.java
@@ -8,11 +8,14 @@ package com.framework.WorldOfZuul;
 public class Guard {
     private Room currentRoom;
     private Room[] patrol;
+    private int numOfRooms = 0;
     private int roomsToPatrol = 0;
     
     public Guard(Room room){
         this.currentRoom = room;
         patrol = new Room[10];
+        patrol[numOfRooms] = room;
+        numOfRooms++;
     }
 
     public Room getCurrentRoom() {
@@ -20,11 +23,21 @@ public class Guard {
     }
     
     public void addToPatrol(Room room){
-        if (roomsToPatrol == patrol.length) {
+        if (numOfRooms == patrol.length) {
             System.out.println("A guard can only guard so many rooms");
         } else {
-            patrol[roomsToPatrol] = room;
-            roomsToPatrol++;
+            patrol[numOfRooms] = room;
+            numOfRooms++;
+        }
+    }
+    public void moveToNextRoom(){
+        if (numOfRooms <= 1) {
+            System.out.println("A guard only have one room, so can't move to another one");
+        } else if (roomsToPatrol+1 <= numOfRooms) {
+            currentRoom = patrol[++numOfRooms];
+        }
+ {
+            
         }
     }
 }
