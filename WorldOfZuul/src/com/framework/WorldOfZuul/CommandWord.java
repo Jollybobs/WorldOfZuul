d8004bd4a3e351e14797fb3d4b1bbae6bfe02e4b
jphj <jphj@LT01410.terma.com>
2017-10-24 12:51:24 +0200

    Commentation

--- WorldOfZuul/src/com/framework/WorldOfZuul/CommandWord.java
+++ WorldOfZuul/src/com/framework/WorldOfZuul/CommandWord.java
@@ -1,6 +1,10 @@
 package com.framework.WorldOfZuul;
 
 /**
+ * An Enum designed to guard the list of usable commandWords from things like spelling mistakes etc. when used in the code.
+ * Furthermore many IDE's has the abilities to show a list of possible enums when using a method which has this enum aas an input argument.
+ * This can not be done with a String, forcing the developer to look inside the method an backward-engineer the possibilities.
+ * 
  * @author Michael Kolling and David J. Barnes
  * @version 2006.03.30
  */
--- WorldOfZuul/src/com/framework/WorldOfZuul/Game.java
+++ WorldOfZuul/src/com/framework/WorldOfZuul/Game.java
@@ -9,16 +9,26 @@ public class Game
     private Parser parser;
     private Room currentRoom;
         
+    /**
+     * Constructor for Game Class.
+     * At instantiation the rooms are created and a parser for handling the command line input is instantiated.
+     * After instantiation call .play() to start the game
+     */
     public Game() 
     {
         createRooms(); 
         parser = new Parser();
     }
 
+    /**
+     * Creates room instantiations, sets exits in rooms and defines the starting room.
+     */
     private void createRooms()
     {
+        // Defines room variables.
         Room cell, southHall, hall, northHall, office, canteen, yard, workshop, bathroom, sewer, sewerExit, freedom;
       
+        // Instantiates room variables.
         cell = new Room("in your cell, the door to the hall is to the east");
         southHall = new Room("in the southern end of the hall, the hall keeps going north and the door to the bathroom is to the east");
         hall = new Room("in the central part of the hall, the hall keeps going both south and north, the door the the yard is to the east");
@@ -32,7 +42,7 @@ public class Game
         sewerExit = new Room("in the sewers, the sewer keeps going to the west but you see an exit above you and back east is the sewer at your cell");
         freedom = new Room("free!");
         
-        
+        // Uses .setExit() method to define exits in rooms. 
         cell.setExit("east", southHall);
 
         southHall.setExit("west", cell);
@@ -66,21 +76,33 @@ public class Game
         sewerExit.setExit("east", sewer);
         sewerExit.setExit("up", sewer);
         
+        // Sets a room-variable to the membervariable currentRoom. Thus defining a starting room.
         currentRoom = cell;
     }
 
+    /**
+     * Method to handle game loop and shut down. Initialization is handles by the Game constructor itself.
+     */
     public void play() 
     {            
         printWelcome();
 
+        // Sets boolean variable finished to false to maintain the game loop until a shutdown is initiated from inside the loop, by setting the boolean to true.
         boolean finished = false;
         while (! finished) {
+            // Inside the loop a command from the command line is received after being parsed by the Parser class instantiation saved in a membervariable.
             Command command = parser.getCommand();
+            // The command is then processed by the processCommand method and returns a boolean to the variable finished.
             finished = processCommand(command);
+            // if the command is to set finished = true the loop ends, else the loop will continue to run.
         }
+        // After the loop, a shut down message is displayed.
         System.out.println("Thank you for playing.  Good bye.");
     }
 
+    /**
+     * Prints a welcome message to the console.
+     */
     private void printWelcome()
     {
         System.out.println();
@@ -91,23 +113,39 @@ public class Game
         System.out.println(currentRoom.getLongDescription());
     }
 
+    /**
+     * Processes a command object by using the objects .getCommandWord() method to retrieve the first word in the command supplied by the user.
+     * This CommandWord dictates which operation is to be executed.
+     * 
+     * @param command a Command Object.
+     * @return boolean wantToQuit, if true the program shuts down.
+     */
     private boolean processCommand(Command command) 
     {
+        // Starts by setting the return boolean to false. This is to ensure that the program won't accidently shut down.
         boolean wantToQuit = false;
 
+        // The CommandWord is fetched from the Command object.
         CommandWord commandWord = command.getCommandWord();
 
+        // Firstly a guard to check for a unknown CommandWord is applied. This is to give user feedback.
+        // If this is outcommented nothing would happen when the user types an unknown command.
+        // The call printHelp() is added to supply the user with a list of usable commands, 
+        // because perhaps the user has forgotten that help is a useable command.
         if(commandWord == CommandWord.UNKNOWN) {
             System.out.println("I don't know what you mean...");
+            printHelp();
             return false;
         }
 
+        // an if/else block to apply function calls to the correct CommandWord.
         if (commandWord == CommandWord.HELP) {
             printHelp();
         }
         else if (commandWord == CommandWord.GO) {
             goRoom(command);
         }
+        // if CommandWord == commandWord.QUIT, the wantToQuit variable is changed to true and the program will shut down.
         else if (commandWord == CommandWord.QUIT) {
             wantToQuit = quit(command);
         }
@@ -117,6 +155,9 @@ public class Game
         return wantToQuit;
     }
 
+    /**
+     * Prints a helping guide to the console, indicating which commands are available at the current moment.
+     */
     private void printHelp() 
     {
         System.out.println("You wander around the prison and");
@@ -126,30 +167,48 @@ public class Game
         parser.showCommands();
     }
 
+    /**
+     * if commandWord == CommandWord.GO the private goRoom method is executed.
+     * supplied with the Command object this method extracts the Secondword.
+     * This is used to determine the direction to execute the goRoom method in.
+     * 
+     * @param command 
+     */
     private void goRoom(Command command) 
     {
+        // Null-point Guard to make sure the Command object has a SecondWord.
+        // If the SecondWord is not defined in memory by a pointer, as soon as
+        // the program tries to access the memory at the end of the undefined pointer
+        // the program will crash if the Null-pointer exception isn't handed by a try/catch.
         if(!command.hasSecondWord()) {
             System.out.println("Go where?");
             return;
         }
 
+        // Extracts the SecondWord from the Command object
         String direction = command.getSecondWord();
 
+        // Sets nextRoom to the desired next room
         Room nextRoom = currentRoom.getExit(direction);
 
+        // A hidden Null-pointer guard, wich display a message to the user, if there are no door.
         if (nextRoom == null) {
             System.out.println("There is no door!");
         }
+        // If there is a door, the currentRoom is set to nextRoom and a LongDescription af the now currentRoom is displayed to the user.
         else {
             currentRoom = nextRoom;
             System.out.println(currentRoom.getLongDescription());
         }
     }
+    
+    // TODO - Unfinished method.
     private void lookRoom(){
         System.out.println(currentRoom.getLongDescription());
         //code to view the items in the room
     }
 
+    // In order to quit, a second word has to be applied. THIS FUNCTIONALITY HOWEVER SEEMS TO BE BROKEN!!.
     private boolean quit(Command command) 
     {
         if(command.hasSecondWord()) {
