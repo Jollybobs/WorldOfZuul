44774837e67e115dfea343b34f57d7686622b7f5
103020 <kierkegaardchristian@gmail.com>
2017-10-29 13:37:09 +0100

    Added some comments to the methodes in Guard

--- WorldOfZuul/src/com/framework/WorldOfZuul/Guard.java
+++ WorldOfZuul/src/com/framework/WorldOfZuul/Guard.java
@@ -11,6 +11,10 @@ public class Guard {
     private int numOfRooms = 0;
     private int roomsToPatrol = 0;
     
+    /**
+     * This class's constuctor
+     * @param room the room the guard starts in
+     */
     public Guard(Room room){
         this.currentRoom = room;
         patrol = new Room[10];
@@ -18,18 +22,30 @@ public class Guard {
         numOfRooms++;
     }
 
+    /**
+     * A getter 
+     * @return the room the guard is in
+     */
     public Room getCurrentRoom() {
         return currentRoom;
     }
     
+    /**
+     * Addes rooms that the guard will move to when it is called with a methode
+     * @param room the new room that the guard can move to
+     */
     public void addToPatrol(Room room){
-        if (numOfRooms == patrol.length) {
+        if (numOfRooms == patrol.length) { //can be change so the list gets exstended
             System.out.println("A guard can only guard so many rooms");
         } else {
             patrol[numOfRooms] = room;
             numOfRooms++;
         }
     }
+    
+    /**
+     * Moves to the next room on the list but only if there was added a room to the Room[] patrol
+     */
     public void moveToNextRoom(){
         if (numOfRooms <= 1) {
             System.out.println("A guard only have one room, so can't move to another one");
