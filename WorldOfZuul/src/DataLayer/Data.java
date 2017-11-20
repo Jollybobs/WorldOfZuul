e84bbee7f20fb25cff406b8fc5cc8c0fdc6a8365
103020 <kierkegaardchristian@gmail.com>
2017-11-20 14:57:01 +0100

    added a save and load highscore, mayby it is not needed

--- WorldOfZuul/src/DataLayer/Data.java
+++ WorldOfZuul/src/DataLayer/Data.java
@@ -11,6 +11,9 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.nio.file.Files;
+import java.nio.file.LinkOption;
+import java.nio.file.Path;
 import java.util.ArrayList;
 
 /**
@@ -26,7 +29,11 @@ public class Data {
         String folderName = "data";
         
         File f = new File(desktop.toString(),folderName);
-        f.mkdirs();
+        
+        if(!f.exists()){
+            f.mkdirs();
+        }
+        
         try {
             FileOutputStream fileOut = new FileOutputStream(desktop + "\\" + folderName + "\\data.ser");
             ObjectOutputStream out = new ObjectOutputStream(fileOut);
@@ -47,6 +54,54 @@ public class Data {
         
         try {
            FileInputStream fileIn = new FileInputStream(desktop + "\\data\\data.ser");
+           ObjectInputStream in = new ObjectInputStream(fileIn);
+           Object temp = in.readObject();
+            if (temp instanceof ArrayList) {
+                aList = (ArrayList) temp;
+            }
+           in.close();
+           fileIn.close();
+           return aList;
+        } catch (IOException i) {
+           i.printStackTrace();
+        } catch (ClassNotFoundException c) {
+           c.printStackTrace();
+        }
+        return null;
+    }
+    protected boolean saveHighscore(ArrayList aList){
+        
+        File desktop = new File(System.getProperty("user.home"), "Desktop");
+        //System.out.println(desktop.toString());
+        
+        String folderName = "data";
+        
+        File f = new File(desktop.toString(),folderName);
+       
+        if(!f.exists()){
+            f.mkdirs();
+        }
+        
+        try {
+            FileOutputStream fileOut = new FileOutputStream(desktop.toString() + "\\" + folderName + "\\high.ser");
+            ObjectOutputStream out = new ObjectOutputStream(fileOut);
+            out.writeObject(aList);
+            out.close();
+            fileOut.close();
+            return true;
+        } catch (IOException i) {
+            i.printStackTrace();
+        }
+        return false;
+    }
+    protected ArrayList loadHighscore(){
+        ArrayList aList = new ArrayList();
+        
+        File desktop = new File(System.getProperty("user.home"), "Desktop");
+        //System.out.println(desktop.toString());
+        
+        try {
+           FileInputStream fileIn = new FileInputStream(desktop + "\\data\\high.ser");
            ObjectInputStream in = new ObjectInputStream(fileIn);
            Object temp = in.readObject();
             if (temp instanceof ArrayList) {
