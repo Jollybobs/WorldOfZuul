2cda999735a18cb01f02b016d1b82396db3f3992
103020 <kierkegaardchristian@gmail.com>
2017-12-04 12:28:29 +0100

    disabled the controll button on the menu

--- WorldOfZuul/src/UI/FXMLDocumentController.java
+++ WorldOfZuul/src/UI/FXMLDocumentController.java
@@ -14,6 +14,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
@@ -70,9 +71,24 @@ public class FXMLDocumentController implements Initializable {
     private ImageView tile07;
     @FXML
     private ImageView tile08;
+    @FXML
+    private Button buttonNorth;
+    @FXML
+    private Button buttonWest;
+    @FXML
+    private Button buttonSouth;
+    @FXML
+    private Button buttonWait;
+    @FXML
+    private Button buttonEast;
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-
+        buttonEast.setDisable(true);
+        buttonNorth.setDisable(true);
+        buttonSouth.setDisable(true);
+        buttonWait.setDisable(true);
+        buttonWest.setDisable(true);
     }
 
     @FXML
