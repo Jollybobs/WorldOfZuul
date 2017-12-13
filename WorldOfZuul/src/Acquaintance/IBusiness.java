dbe49ad00e0c39cc49a4b517b4afba8a6d551f3f
thoreyjons <thorey.jonsdottir@gmail.com>
2017-12-13 11:52:06 +0100

    IBusiness

--- WorldOfZuul/src/Acquaintance/IBusiness.java
+++ WorldOfZuul/src/Acquaintance/IBusiness.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Acquaintance;
 
 import Business.Game;
@@ -10,7 +5,7 @@ import java.util.List;
 
 /**
  *
- * @author Bruger
+ * List of method that are used for allowing communication between the UI and the business layer
  */
 public interface IBusiness {
 
